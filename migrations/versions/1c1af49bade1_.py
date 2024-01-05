a64a3b0b7d91fea7505ac42a62cf91c0621d4f03
Dslayton1998 <dslayton1998@gmail.com>
2024-01-05 14:27:20 -0600

    Finished create new blog, still needs a little work

--- migrations/versions/1c1af49bade1_.py
+++ migrations/versions/1c1af49bade1_.py
@@ -1,20 +1,16 @@
 """empty message
 
-Revision ID: 5a1c8fb7b77a
+Revision ID: 1c1af49bade1
 Revises: 
-Create Date: 2024-01-04 12:31:12.808307
+Create Date: 2024-01-05 14:12:03.361366
 
 """
 from alembic import op
 import sqlalchemy as sa
 
-import os
-environment = os.getenv("FLASK_ENV")
-SCHEMA = os.environ.get("SCHEMA")
-
 
 # revision identifiers, used by Alembic.
-revision = '5a1c8fb7b77a'
+revision = '1c1af49bade1'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -37,9 +33,9 @@ def upgrade():
     sa.Column('blog_name', sa.String(length=50), nullable=False),
     sa.Column('owner_id', sa.Integer(), nullable=True),
     sa.Column('profile_picture', sa.String(length=255), nullable=False),
-    sa.Column('background_image', sa.String(length=255), nullable=True),
-    sa.Column('primary_blog', sa.Boolean(), nullable=False),
-    sa.Column('public', sa.Boolean(), nullable=False),
+    sa.Column('background_image', sa.String(length=255), nullable=False),
+    sa.Column('primary_blog', sa.Boolean(), nullable=True),
+    sa.Column('public', sa.Boolean(), nullable=True),
     sa.ForeignKeyConstraint(['owner_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('blog_name')
@@ -54,11 +50,6 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
-
-    if environment == "production":
-        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
-        op.execute(f"ALTER TABLE blogs SET SCHEMA {SCHEMA};")
-        op.execute(f"ALTER TABLE posts SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
