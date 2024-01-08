d7bcea11b6266b24a60a7ff7d856232d0b0c5f40
Dslayton1998 <dslayton1998@gmail.com>
2024-01-08 12:27:38 -0600

    Finished post creation, fixed blog create

---
+++ app/forms/post_form.py
@@ -0,0 +1,12 @@
+from flask_wtf.file import FileRequired, FileAllowed
+from wtforms.validators import DataRequired
+from wtforms import StringField, FileField, IntegerField
+from flask_wtf import FlaskForm
+
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+
+class PostForm(FlaskForm):
+    image = FileField('Image', validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
+    caption = StringField('Caption')
+    blog_id = IntegerField('Blog id', validators=[DataRequired()])
+    user_id = IntegerField('User id', validators=[DataRequired()])
