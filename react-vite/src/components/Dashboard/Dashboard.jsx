db11f20c504572938c8529feef2e1571ba9f86bc
Dslayton1998 <dslayton1998@gmail.com>
2024-01-11 14:17:59 -0600

    Navbar almost done, moving on to blog-page

--- react-vite/src/components/Dashboard/Dashboard.jsx
+++ react-vite/src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { thunkAllPosts } from "../../redux/post";
 import PostsCards from "./PostsCards";
-// import { thunkAllBlogs } from "../../redux/blog";
+import './Dashboard.css'
 
 export default function Dashboard() {
     //todo: render posts as cards, rest of the dashboard comes later
@@ -19,7 +19,8 @@ export default function Dashboard() {
 
     return (
         <>
-        <div>
+        <div className="dashboard-container">
+            <h1 className="dashboard-header">Dashboard</h1>
             {allPosts.map(post => (
                 <PostsCards post={post} key={post.id}/>
             ))}
---
+++ react-vite/src/components/MyBlogs/MyBlogs.jsx
@@ -0,0 +1,12 @@
+export default function MyBlogs() {
+    /*
+    query for blogs matching current users ID
+    render blog cards with user options 
+    */
+
+    return (
+        <>
+        this will be my blogs page
+        </>
+    )
+}
