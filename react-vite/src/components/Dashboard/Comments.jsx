810b76c0f83db19c5ed2905940da08c549087bc7
Dslayton1998 <dslayton1998@gmail.com>
2024-02-26 18:33:48 -0600

    small change today

--- react-vite/src/components/Dashboard/Comments.jsx
+++ react-vite/src/components/Dashboard/Comments.jsx
@@ -1,21 +1,22 @@
-//todo: take functionality from PostCards to clean up code and render all comments
+//todo: Need blog information to give an option to comment under x blog name
 
-import { useDispatch } from "react-redux"
 import { thunkDeleteComment } from "../../redux/post";
+import { useDispatch } from "react-redux";
 
 export default function Comments({ comment, blog }) {
     const dispatch = useDispatch();
 
-    console.log(comment)
+    // console.log(comment)
+
 
     const onClick = () => {
-        dispatch(thunkDeleteComment(commentObj.id))
+        dispatch(thunkDeleteComment(comment.id))
     } 
     
     return (
         <div className="comment">
-        {comment.comment}
-        <button onClick={onClick}>Delete</button>
+            {comment.comment}
+            <button onClick={onClick}>Delete</button>
         </div>
     )
 }
