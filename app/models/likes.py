5df75d1ca2377937be55a254878bee947dd223bc
Dslayton1998 <dslayton1998@gmail.com>
2024-03-27 16:53:24 -0500

    model created for likes

--- app/models/likes.py
+++ app/models/likes.py
@@ -9,3 +9,19 @@ class Like(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
     post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("posts.id")))
+
+    post = db.relationship("Post", back_populates="likes")
+
+    def to_dict(self, printer=False):
+        return_dict = {
+            "id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+            # "post": self.post.to_dict()
+            # ^ Might not need post in this to_dict
+        }
+
+        if printer:
+            print(return_dict)
+
+        return return_dict
